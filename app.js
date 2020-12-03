926957cf47ccbd75817e176b75a5284d701d5d04
TJ <Cyr-@users.noreply.github.com>
2020-12-02 18:42:23 -0600

    Creation of script

---
+++ app.js
@@ -0,0 +1,273 @@
+const axios = require("axios");
+
+function toTitleCase(str) {
+    return str.replace(/\b\w+/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+let db = "";
+
+axios
+    .get("https://sw5eapi.azurewebsites.net/api/equipment")
+    .then((response) => response.data)
+    .then((data) => {
+        data.filter((entry) => entry.equipmentCategory === "Weapon" && entry.contentSource === "PHB").map((entry) => {
+            const dbObject = {};
+
+            // Uncomment for debugging to display all of the api entries
+            // console.log(entry);
+
+            // Name
+            dbObject.name = toTitleCase(entry.name);
+
+            // Permission
+            dbObject.permission = { default: 0, vXYkFWX6qzvOu2jc: 3 };
+
+            // Type
+            dbObject.type = "weapon";
+
+            // Description
+            let descriptionValue = "";
+
+            const propertiesValue = [
+                entry.propertiesMap.Burst && toTitleCase(entry.propertiesMap.Burst),
+                entry.propertiesMap.Dexterity && toTitleCase(entry.propertiesMap.Dexterity),
+                entry.propertiesMap.Double && toTitleCase(entry.propertiesMap.Double),
+                entry.propertiesMap.Rapid && toTitleCase(entry.propertiesMap.Rapid),
+                entry.propertiesMap.Reload && toTitleCase(entry.propertiesMap.Reload),
+                entry.propertiesMap.Strength && toTitleCase(entry.propertiesMap.Strength)
+            ]
+                .filter(Boolean)
+                .join(", ");
+
+            if (propertiesValue) {
+                descriptionValue += `<p>${propertiesValue}</p>`;
+            }
+
+            if (entry.description) {
+                descriptionValue += `<p>${entry.description}</p>`;
+            }
+
+            dbObject.description = {
+                value: descriptionValue,
+                chat: "",
+                unidentified: ""
+            };
+
+            // Source
+            dbObject.source = entry.contentSource;
+
+            // Quantity
+            dbObject.quantity = 1;
+
+            // Weight
+            dbObject.weight = entry.Weight;
+
+            // Price
+            dbObject.price = entry.Cost;
+
+            // Attuned
+            dbObject.attuned = false;
+
+            // Equipped
+            dbObject.equipped = false;
+
+            // Rarity
+            dbObject.rarity = false;
+
+            // Identified
+            dbObject.identified = true;
+
+            // Activation
+            dbObject.activation = {
+                type: "action",
+                cost: 1,
+                condition: ""
+            };
+
+            // Duration
+            dbObject.duration = {
+                value: null,
+                units: ""
+            };
+
+            // Target
+            dbObject.duration = {
+                value: 1,
+                width: null,
+                units: "",
+                type: "enemy"
+            };
+
+            // Range
+            const rangeString =
+                (entry.propertiesMap.Ammunition && entry.propertiesMap.Ammunition.match(/\d+\/\d+/g)) ||
+                (entry.propertiesMap.Range && entry.propertiesMap.Range.match(/\d+\/\d+/g));
+            let rangeValues = { value: null, long: null, units: "" };
+
+            if (rangeString) {
+                const rangeSplit = rangeString[0].split("/");
+
+                rangeValues = { value: rangeSplit[0], long: rangeSplit[1], units: "ft" };
+            }
+
+            dbObject.range = {
+                ...rangeValues
+            };
+
+            // Uses
+            dbObject.uses = {
+                value: 0,
+                max: 0,
+                per: ""
+            };
+
+            // Consume
+            if (entry.weaponClassification.includes("Blaster")) {
+                dbObject.consume = {
+                    type: "ammo",
+                    target: "",
+                    amount: 1
+                };
+            } else {
+                dbObject.consume = {
+                    type: "",
+                    target: "",
+                    amount: null
+                };
+            }
+
+            // Ability
+            dbObject.ability = "";
+
+            // Action Type
+            dbObject.actionType =
+                (entry.propertiesMap.Ammunition && entry.propertiesMap.Ammunition.match(/range/g)) ||
+                (entry.propertiesMap.Range && entry.propertiesMap.Range.match(/range/g))
+                    ? "rwak"
+                    : "mwak";
+
+            // Attack Bonus
+            dbObject.attackBonus = "";
+
+            // Chat Flavor
+            dbObject.chatFlavor = "";
+
+            // Critical
+            dbObject.critical = null;
+
+            // Damage
+            const versatileValue = entry.propertiesMap.Versatile && entry.propertiesMap.Versatile.match(/\d+d\d+/g);
+
+            dbObject.damage = {
+                parts: entry.damageNumberOfDice
+                    ? [[`${entry.damageNumberOfDice}d${entry.damageDieType} + @mod`, entry.damageType.toLowerCase()]]
+                    : [],
+                versatile: versatileValue ? `${versatileValue[0]} + @mod` : ""
+            };
+
+            // Formula
+            dbObject.formula = "";
+
+            // Save
+            dbObject.save = {
+                ability: "",
+                dc: null,
+                scaling: "power"
+            };
+
+            // Weapon Type
+            switch (entry.Type) {
+                case "SimpleBlaster":
+                    dbObject.weaponType = "simpleB";
+                    break;
+
+                case "MartialBlaster":
+                    dbObject.weaponType = "martialB";
+                    break;
+
+                case "SimpleVibroweapon":
+                    dbObject.weaponType = "simpleVW";
+                    break;
+
+                case "MartialVibroweapon":
+                    dbObject.weaponType = "martialVW";
+                    break;
+
+                case "SimpleLightweapon":
+                    dbObject.weaponType = "simpleLW";
+                    break;
+
+                case "MartialLightweapon":
+                    dbObject.weaponType = "martialLW";
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Properties
+            dbObject.properties = {
+                amm: entry.propertiesMap.hasOwnProperty("Ammunition"),
+                aut: entry.propertiesMap.hasOwnProperty("Auto"),
+                bur: entry.propertiesMap.hasOwnProperty("Burst"),
+                def: entry.propertiesMap.hasOwnProperty("Defensive"),
+                dex: entry.propertiesMap.hasOwnProperty("Dexterity"),
+                dir: entry.propertiesMap.hasOwnProperty("Dire"),
+                drm: entry.propertiesMap.hasOwnProperty("Disarming"),
+                dgd: entry.propertiesMap.hasOwnProperty("Disguised"),
+                dis: entry.propertiesMap.hasOwnProperty("Disintegrate"),
+                dpt: entry.propertiesMap.hasOwnProperty("Disruptive"),
+                dou: entry.propertiesMap.hasOwnProperty("Double"),
+                fin: entry.propertiesMap.hasOwnProperty("Finesse"),
+                fix: entry.propertiesMap.hasOwnProperty("Fixed"),
+                foc: entry.propertiesMap.hasOwnProperty("Focus"),
+                hvy: entry.propertiesMap.hasOwnProperty("Heavy"),
+                hid: entry.propertiesMap.hasOwnProperty("Hidden"),
+                ken: entry.propertiesMap.hasOwnProperty("Keen"),
+                lgt: entry.propertiesMap.hasOwnProperty("Light"),
+                lum: entry.propertiesMap.hasOwnProperty("Luminous"),
+                mig: entry.propertiesMap.hasOwnProperty("Mighty"),
+                pic: entry.propertiesMap.hasOwnProperty("Piercing"),
+                rap: entry.propertiesMap.hasOwnProperty("Rapid"),
+                rch: entry.propertiesMap.hasOwnProperty("Reach"),
+                rel: entry.propertiesMap.hasOwnProperty("Reload"),
+                ret: entry.propertiesMap.hasOwnProperty("Returning"),
+                shk: entry.propertiesMap.hasOwnProperty("Shocking"),
+                sil: entry.propertiesMap.hasOwnProperty("Silent"),
+                spc: entry.propertiesMap.hasOwnProperty("Special"),
+                str: entry.propertiesMap.hasOwnProperty("Strength"),
+                thr: entry.propertiesMap.hasOwnProperty("Thrown"),
+                two: entry.propertiesMap.hasOwnProperty("Two-Handed"),
+                ver: entry.propertiesMap.hasOwnProperty("Versatile"),
+                vic: entry.propertiesMap.hasOwnProperty("Vicious")
+            };
+
+            // Proficient
+            dbObject.proficient = false;
+
+            // CP Tooltip Mode
+            dbObject.cptooltipmode = "hid";
+
+            // Flags
+            dbObject.flags = {
+                dynamiceffects: {
+                    equipActive: false,
+                    alwaysActive: false
+                }
+            };
+
+            // Effects
+            dbObject.effects = [];
+
+            // Image
+            dbObject.img = `systems/sw5e/packs/Icons/${entry.weaponClassification
+                .split(/(?=[A-Z])/)
+                .join("%20")}/${dbObject.name.replace(" ", "%20")}.webp`;
+
+            db += JSON.stringify(dbObject);
+        });
+
+        console.log(db);
+    });
